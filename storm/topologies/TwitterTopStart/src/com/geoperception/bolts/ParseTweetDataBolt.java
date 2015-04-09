e380c07c93cc22119ba8a1cc1cab4207d0222bba
johnluke <jl.bucuvalas@gmail.com>
2015-04-09 15:16:57 -0600

    added bolts for limiting by geotag and parsing tweet data

---
+++ storm/topologies/TwitterTopStart/src/com/geoperception/bolts/ParseTweetDataBolt.java
@@ -0,0 +1,42 @@
+package com.geoperception.bolts;
+
+import backtype.storm.topology.BasicOutputCollector;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseBasicBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import twitter4j.HashtagEntity;
+import twitter4j.Status;
+
+import java.util.Date;
+
+/**
+ * Created by johnluke on 4/9/15.
+ */
+public class ParseTweetDataBolt extends BaseBasicBolt {
+    @Override
+    public void execute(Tuple input, BasicOutputCollector collector) {
+        Status status = (Status) input.getValueByField("geotweet");
+        long id = status.getId();
+        String content = status.getText();
+        String uName = status.getUser().getName();
+        Date createdAt = status.getCreatedAt();
+        double lat = status.getGeoLocation().getLatitude();
+        double lng = status.getGeoLocation().getLatitude();
+        String city = status.getPlace().getName();
+        HashtagEntity[] hashtags = status.getHashtagEntities();
+
+        String[] hashTagText = new String[hashtags.length];
+        for (int i = 0; i < hashtags.length; i ++){
+            hashTagText[i] = hashtags[i].getText();
+        }
+
+        collector.emit(new Values(id,content,uName, createdAt, lat,lng,city,hashTagText));
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields("id", "content", "userName", "createdAt", "lat", "lng", "city", "hashTagtext"));
+    }
+}
